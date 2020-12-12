a02d92b25f131c4b41356fb5370e3654e8aebd2a
Shenaj Marla mshenaj@gmail.com <mshenaj@gmail.com>
2020-12-12 11:49:55 +0100

    show photos

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,30 +1,37 @@
 class OrdersController < ApplicationController
 
+
+  def my_orders
+   @my_orders = Order.all
+   @total_orders = @my_orders.map(&:total_pay).sum
+  end
+
   def create
-  creation = Creation.find(params[:creation_id])
-  order  = Order.create!(creation: creation, amount: creation.price, state: 'pending')
+    creation = Creation.find(params[:creation_id])
+    order  = Order.create!(creation: creation, amount: creation.price, quantity: creation.quantity, state: 'pending')
 
-  session = Stripe::Checkout::Session.create(
-    billing_address_collection: 'required',
-    shipping_address_collection: {
-    allowed_countries: ['BE'],
-  },
-    payment_method_types: ['card', 'bancontact'],
-    line_items: [{
-      name: creation.name,
-      amount: creation.price_cents,
-      currency: 'eur',
-      quantity: 1
-    }],
-    success_url: order_url(order),
-    cancel_url: order_url(order)
-  )
+    session = Stripe::Checkout::Session.create(
+      billing_address_collection: 'required',
+      shipping_address_collection: {
+      allowed_countries: ['BE'],
+    },
+      payment_method_types: ['card', 'bancontact'],
+      line_items: [{
+        name: creation.name,
+        amount: creation.price_cents,
+        currency: 'eur',
+        quantity: creation.quantity
+      }],
+      success_url: order_url(order),
+      cancel_url: order_url(order)
+    )
 
-  order.update(checkout_session_id: session.id)
-  redirect_to new_order_payment_path(order)
+    order.update(checkout_session_id: session.id)
+    redirect_to new_order_payment_path(order)
 end
 
 def show
   @order = Order.find(params[:id])
 end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,13 @@ class Order < ApplicationRecord
 
   belongs_to :creation
   monetize :amount_cents
+
+  def total_pay
+    total = 0
+    self.creations.each do |creation|
+      next if creation.quantity.nil?
+      total += creation.quantity * creation.price
+    end
+    return total
+  end
 end
