d10cbc0ee69a0b5629ddde927f62cfccb152f1b1
Shenaj Marla mshenaj@gmail.com <mshenaj@gmail.com>
2021-02-12 11:21:44 +0100

    stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -10,3 +10,4 @@ StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 StripeEvent.configure do |events|
   events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
 end
+
